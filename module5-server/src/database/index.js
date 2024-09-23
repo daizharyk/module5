bb1031d1191324324bfb804684d30bff9b53c14c
daizharyk <aizharyk76@gmail.com>
2024-09-23 10:27:06 +0500

    env

--- module5-server/src/database/index.js
+++ module5-server/src/database/index.js
@@ -1,8 +1,7 @@
 const mongoose = require("mongoose");
 
 module.exports = async () => {
-  await mongoose.connect(
-    "mongodb+srv://didar:didar@cluster0.niptg.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
-  );
+  
+  await mongoose.connect(process.env.MONGO_CONNECTION);
   console.log("MongoDb connected");
 };
--- module5-server/src/index.js
+++ module5-server/src/index.js
@@ -1,3 +1,5 @@
+require("dotenv").config();
+
 const express = require("express");
 const errorHandler = require("../src/infrastructure/errorHandle.js");
 const app = express();
