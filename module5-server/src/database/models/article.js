955ab973fb5e350f206e38ab379a3786e215f6f2
daizharyk <aizharyk76@gmail.com>
2024-09-23 14:02:29 +0500

    Создание моделей БД

---
+++ module5-server/src/database/models/article.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+
+const articleSchema = mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    text: {
+      type: String,
+      required: true,
+    },
+    isDeleted: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("Article", articleSchema);
