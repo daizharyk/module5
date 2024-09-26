db00dff47b7f7a54c8bee1a9f18b8aa4a3532de2
daizharyk <aizharyk76@gmail.com>
2024-09-26 09:34:20 +0500

    закрыть части приложения от неавторизованных пользователей

--- module5-server/src/api/articles/routes.js
+++ module5-server/src/api/articles/routes.js
@@ -1,18 +1,19 @@
 const { Router } = require("express");
 const { getAllArticles, creatNewArticle, getArticle, updateArticle, removeArticle, removeArticleForce } = require("./controller");
 const router = Router();
+const {protected} = require("../../middlewares/auth")
 
-router.get("/", getAllArticles);
+router.get("/",  getAllArticles);
 
-router.post("/", creatNewArticle);
+router.post("/", protected, creatNewArticle);
 
 router.get("/:id", getArticle);
 
-router.put("/:id", updateArticle);
+router.put("/:id",protected, updateArticle);
 
-router.delete("/:id", removeArticle);
+router.delete("/:id", protected,removeArticle);
 
-router.delete("/force/:id", removeArticleForce);
+router.delete("/force/:id",protected, removeArticleForce);
 
 
 
--- module5-server/src/middlewares/auth.js
+++ module5-server/src/middlewares/auth.js
@@ -1,10 +1,11 @@
 const jwt = require("jsonwebtoken");
 const userRepository = require("../repository/userRepository");
 const InvalidDataError = require("../infrastructure/errors/InvalidDataError");
+const NotImplementedError = require("../infrastructure/errors/NotImplementedError");
 module.exports = {
   auth: async (req, res, next) => {
     try {
-      const token = req.headers.authorization.split(" ")[1];
+      const token = req.headers.authorization?.split(" ")[1];
       if (token) {
         const decoded = jwt.verify(token, process.env.JWT_KEY);
         req.user = await userRepository.findUser(decoded.userId);
@@ -13,5 +14,11 @@ module.exports = {
     } catch (error) {
       next(new InvalidDataError("Authentication failed"));
     }
+  },
+  protected: (req, res, next) => {
+    if (!req.user) {
+      next(new NotImplementedError());
+    }
+    next();
   },
 };
